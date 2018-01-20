932ecdbe7720cf1635964eb6a28951331ddd20e0
Alejandro Ballesteros <ayb.021@gmail.com>
2018-01-20 02:45:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,4 @@ var chocolateBars = ["snickers" , "hundred grand" , "kitkat", "skittles"];
 
 addElementToBeginningOfArray(cars, challenger){
   var cars = ["mustang", "charger", "camaro"];
-};
-
-  
+}
